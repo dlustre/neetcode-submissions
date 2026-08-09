4f789ee3bb1e8577680df173a482851a072f525f
Dennis Lustre <dennis.lustre@gmail.com>
2026-08-09 15:29:40 -0700

    Add: products-of-array-discluding-self - submission-13

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-13.py
@@ -0,0 +1,34 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        # store products from left to mid, and products from right to mid
+        fromLeft = []
+        fromRight = []
+
+        for i, num in enumerate(nums):
+            if i == 0:
+                fromLeft.append(num)
+            else:
+                fromLeft.append(num * fromLeft[-1])
+        
+        for i, num in enumerate(reversed(nums)):
+            if i == 0:
+                fromRight.append(num)
+            else:
+                fromRight.append(num * fromRight[-1])
+
+        fromRight.reverse()
+
+        result = []
+
+        # print(fromLeft)
+        # print(fromRight)
+
+        for i in range(len(nums)):
+            if i == 0:
+                result.append(fromRight[i + 1])
+            elif i == len(nums) - 1:
+                result.append(fromLeft[i - 1])
+            else:
+                result.append(fromLeft[i - 1] * fromRight[i + 1])
+
+        return result
